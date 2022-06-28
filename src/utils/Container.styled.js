71bb18f6463d5f4ceee64971e8874bac28fbf45a
vitalichUA <88967645+vitalichUA@users.noreply.github.com>
2022-06-28 17:56:14 +0000

    Deploying to gh-pages from @ vitalichUA/goit-react-hw-01-components@3cac68323038a860df67764b68fa4168c96f3f19 🚀

--- src/utils/Container.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-padding: ${p => p.theme.space[5]}px
-`
